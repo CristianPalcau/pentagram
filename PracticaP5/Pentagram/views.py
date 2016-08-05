9b0598183eb0e5644208ebebc373cb767bc9753c
CristianPalcau <palcau.cristi@gmail.com>
2016-08-05 14:08:50 +0300

    last day commit

--- PracticaP5/Pentagram/views.py
+++ PracticaP5/Pentagram/views.py
@@ -2,7 +2,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
-from Pentagram.models import Photo, Comment, Like
+from Pentagram.models import Photo, Comment, Like, User
 from Pentagram.serializers import PhotoSerializer, UserSerializer, CommentSerializer
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
@@ -23,7 +23,7 @@ def photos(request):
         return Response(status=status.HTTP_400_BAD_REQUEST, data=photo_serializer.errors)
 
 
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 @permission_classes((AllowAny,))
 def users(request):
     if request.method == 'POST':
@@ -32,6 +32,10 @@ def users(request):
             user_serializer.save()
             return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST, data=user_serializer.errors)
+    if request.method == "GET":
+        all_users = User.objects.all()
+        user_serializer = UserSerializer(all_users, many=True)
+        return Response(status=status.HTTP_200_OK, data=user_serializer.data)
 
 
 @api_view(['GET', 'POST'])
